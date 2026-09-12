c7639940ca054ae8de94d0e9aacb1e4f48ddc191
botgerr-ops <botger.r@gmail.com>
2026-09-12 18:48:19 +0200

    Build first permanently signed ScreenFlow release v0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "nl.screenflow.player"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.2.1"
+        versionCode = 7
+        versionName = "0.2.2"
     }
 
     signingConfigs {

1033b89853061234e16985a2fa40cd137ff9f1ed
botgerr-ops <botger.r@gmail.com>
2026-09-12 10:31:52 +0200

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "nl.screenflow.player"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "nl.screenflow.player"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}

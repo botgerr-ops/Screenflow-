f2fc4683f499f831b589a99d700279ad3a11f2db
botgerr-ops <botger.r@gmail.com>
2026-09-12 20:04:51 +0200

    Release ScreenFlow Player v0.3.0 with offline playback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "nl.screenflow.player"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.2.2"
+        versionCode = 8
+        versionName = "0.3.0"
     }
 
     signingConfigs {

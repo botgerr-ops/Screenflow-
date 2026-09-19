26c030af85196c4b308af5b2d1460d96ad2083f7
botgerr-ops <botger.r@gmail.com>
2026-09-19 23:17:13 +0200

    TEST: bump fullscreen player build to 0.5.7 with in-place upgrade version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "nl.screenflow.player"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "0.5.6-test"
+        versionCode = 15
+        versionName = "0.5.7-test"
         buildConfigField("String", "PLAYER_API_BASE_URL", "\"https://bqapbwsvfofgnfogwhdx.supabase.co/functions/v1\"")
     }
 
